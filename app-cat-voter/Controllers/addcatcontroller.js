14a1c8fe81112d9d75d3a3e7e41350d2c0b598c6
Sivakumar Manneni <Sivakumar_Manneni@epam.com>
2017-05-17 17:53:33 +0530

    angular tasks added

---
+++ app-cat-voter/Controllers/addcatcontroller.js
@@ -0,0 +1,10 @@
+//need to refractor h-w-1 section 3
+(function(module) {
+  module.controller("addcatcontroller", addcatcontroller);
+
+})(angular.module("catvoter"));
+
+function addcatcontroller($scope) {
+  var vm=this;
+}
+
